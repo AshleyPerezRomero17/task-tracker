9f228570828aee097a5e4c7398d10d637de50dcf
Ashley Perez Romero <AshleyPerez@AdminisatorsMBP.attlocal.net>
2022-10-26 23:42:49 -0400

    Clean up project

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-<!-- Add a ReadMe file describing the process you had to go through when building the application. Also add a section talking about some errors you had and how you solved those errors. -->
- <!-- Project's Title
-description  -->
 # task-tracker
+
+This project consisted of building different components in order to create a todo application:
+
+## API
+Had to create a Node.js using Express to build and API to handle creating a todo, deleting a todo, updating a todo and returning all todos.
+
+#### Routes
+
+| Method | URL       | Description   |
+|--------|-----------|---------------|
+| GET    | /         | Renders index |
+| POST   | /todo     | Creates TODO  |
+| PUT    | /todo/:id | Updates TODO  |
+| DELETE | /todo/:id | Deletes TODO  |
+
+## Views
+Used pug.js, a HTML template engine for Node and integrate it with Express as view engine. For styling used a CSS stylesheet.
+
+## Data
+Used a JSON file and file system to maintain data
+
+## Validation
+Used npm package `express-validator` to add JS validation to form task input. If validation fails then it will return a status code `400 Bad Request` and the response contains the validdation errors.
+
+## Deployment
+- Deploy using AWS
--- index.js
+++ index.js
@@ -1,61 +1,92 @@
 const express = require("express");
 const app = express();
 
+// JS Validation
+const { body, validationResult } = require("express-validator");
+
 app.set("view engine", "pug");
 app.set("views", "views");
 
-const fs = require ("fs");
-const bodyParser = require ("body-parser");
+// override with POST having ?_method=DELETE
+// because HTML form only supports POST and GET methods
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
-// create application/json parser
-var jsonParser = bodyParser.json()
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-const filePath = __dirname + "/data.json"
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const filePath = __dirname + "/data.json";
 const data = fs.readFileSync(filePath);
 const parsedData = JSON.parse(data);
 
-// generateRandomNumber helper function to generate unique number ID 
+// generateRandomNumber helper function to generate unique number ID
 function generateRandomNumber() {
-  return Math.floor(Math.random() * 100)
+  return Math.floor(Math.random() * 100);
 }
 
 app.get(`/`, (req, res) => {
-  res.render(`index`, {tasks: parsedData });
+  res.render(`index`, { tasks: parsedData });
 });
 
 // /todo create todo item
-app.post("/todo", jsonParser, function (req, res) {
-  var todoReqObj = req.body
-  todoReqObj.id = generateRandomNumber();
-  parsedData.push(todoReqObj)
+app.post(
+  "/todo",
+  body("task").notEmpty().withMessage("task description required"),
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  fs.writeFileSync(filePath, JSON.stringify(parsedData))
-  res.sendStatus(201).redirect("/");
-});
+    var todoReqObj = req.body;
+    todoReqObj.id = generateRandomNumber();
+    parsedData.push(todoReqObj);
+
+    fs.writeFileSync(filePath, JSON.stringify(parsedData));
+    res.render(`index`, { tasks: parsedData });
+  }
+);
 
 // Update todo by todo ID
-app.put("/todo/:id", jsonParser, function (req, res) {
-  var indexOfObject = parsedData.findIndex(object => {
-    return object.id == req.params.id
-  })
+app.put(
+  "/todo/:id",
+  body("task").notEmpty().withMessage("task description required"),
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const indexOfObject = parsedData.findIndex((object) => {
+      return object.id == req.params.id;
+    });
 
-  parsedData[indexOfObject] = {
-    ...parsedData[indexOfObject], task: req.body.task
+    parsedData[indexOfObject] = {
+      ...parsedData[indexOfObject],
+      task: req.body.task,
+    };
+
+    fs.writeFileSync(filePath, JSON.stringify(parsedData));
+    res.render(`index`, { tasks: parsedData });
   }
-
-  fs.writeFileSync(filePath, JSON.stringify(parsedData))
-  res.sendStatus(200).redirect("/");
-});
+);
 
 // /todo/:id deletes todo item by todoID
 app.delete("/todo/:id", (req, res) => {
-  var indexOfObject = parsedData.findIndex(object => {
-    return object.id == req.params.id
-  })
-  parsedData.splice(indexOfObject, 1)
+  const indexOfObject = parsedData.findIndex((object) => {
+    return object.id == req.params.id;
+  });
+  parsedData.splice(indexOfObject, 1);
 
-  fs.writeFileSync(filePath, JSON.stringify(parsedData))
-  res.sendStatus(204).redirect("/");
+  fs.writeFileSync(filePath, JSON.stringify(parsedData));
+  res.render(`index`, { tasks: parsedData });
 });
 
 app.listen(3000);
